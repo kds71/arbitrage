5cf0e3f2ca35998cd148b7fe82fd31874f68d28a
kds71 <kds71@MacBook-Pro-kds71.local>
2018-05-07 20:04:55 +0200

    login module

--- www/module/login/main.js
+++ www/module/login/main.js
@@ -3,6 +3,7 @@
 Arb.LoadedModules.login = {
 
     rendered: false,
+    listenersRegistered: false,
 
     init: function() {
 
@@ -11,10 +12,31 @@ Arb.LoadedModules.login = {
             Arb.LoadedModules.login.$container = Arb.Module.renderModuleContainer('login', 'main', {});
         }
 
+        if (!Arb.LoadedModules.login.listenersRegistered) {
+            Arb.LoadedModules.login.listenersRegistered = true;
+            Arb.registerWebsocketListeners(Arb.LoadedModules.login);
+        }
+
         Arb.Module.displayModule('login');
         Arb.LoadedModules.login.$container.find('input').eq(0).focus();
 
-    }
+    },
+
+    loginSuccessListener: signed([
+
+        { message: C.WS_INFO_LOGIN_SUCCESS },
+        function(response) {
+        }
+
+    ]),
+
+    loginFailedListener: signed([
+
+        { message: C.WS_INFO_LOGIN_FAILED },
+        function(response) {
+        }
+
+    ]);
 
 };
 
