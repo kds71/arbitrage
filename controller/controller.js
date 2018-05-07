519f341fc6f70f8690083782642dfb143cc9a6ee
kds71 <kds71@MacBook-Pro-kds71.local>
2018-05-07 11:54:59 +0200

    parsing directories from config

--- controller/controller.js
+++ controller/controller.js
@@ -1,10 +1,27 @@
 'use strict';
 
+var path = require('path');
+
+var C = require('../lib/constants'),
+    AppManager = require('./app-manager');
+
 module.exports = class Controller {
 
     constructor(config) {
 
         this.config = config;
+
+        var dir = config.directories;
+
+        this.directories = {
+            root: dir.root,
+            app: path.join(dir.root, dir.app),
+            log: path.join(dir.root, dir.log),
+            lib: path.join(dir.root, dir.lib),
+            config: path.join(dir.root, dir.config)
+        };
+
+        this.appManager = new AppManager(config.app, this.directories);
 
     }
 
