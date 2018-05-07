58cc0fbc6c8a1b3994ce8a73d727d8eccd5372a7
kds71 <kds71@MacBook-Pro-kds71.local>
2018-05-07 17:35:49 +0200

    frontend application lock

--- www/js/arbitrage.js
+++ www/js/arbitrage.js
@@ -2,7 +2,30 @@
 
 var Arb = {
 
+    locked: false,
+
     init: function() {
+
+        Arb.Websocket.init();
+
+    },
+
+    lock: function() {
+
+        if (!Arb.locked) {
+            Arb.locked = true;
+            $('body').addClass('locked');
+        }
+
+    },
+
+    release: function() {
+
+        if (Arb.locked) {
+            Arb.locked = false;
+            $('body').removeClass('locked');
+        }
+
     }
 
 };
